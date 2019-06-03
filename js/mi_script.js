1a04ad1f7f7a33f001a87d906436fe81d0470716
Sebastián Caggiano <seba2610@gmail.com>
2019-06-03 20:17:01 -0300

    Se finaliza con el proceso de poner en venta un producto.

--- js/mi_script.js
+++ js/mi_script.js
@@ -3,7 +3,11 @@ let productCount = 0;
 let comissionPercentage = 0.13;
 let MONEY_SYMBOL = "$";
 let PERCENTAGE_SYMBOL = '%';
+let URL_PUBLISH_PRODUCT = "https://api.myjson.com/bins/18i9vb";
+let SUCCESS_MSG = "¡Se ha realizado la publicación con éxito! :)";
+let ERROR_MSG = "Ha habido un error:(, verifica qué pasó.";
 
+//Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts(){
     let unitProductCostHTML = document.getElementById("productCostText");
     let comissionCostHTML = document.getElementById("comissionText");
@@ -18,7 +22,9 @@ function updateTotalCosts(){
     totalCostHTML.innerHTML = totalCostToShow;
 }
 
-
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("productCountInput").addEventListener("change", function(){
         productCount = this.value;
@@ -43,5 +49,52 @@ document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("standardradio").addEventListener("change", function(){
         comissionPercentage = 0.03;
         updateTotalCosts();
+    });
+
+    //Se obtiene el formulario de publicación de producto
+    var sellForm = document.getElementById("sell-info");
+
+    //Se agrega una escucha en el evento 'submit' que será
+    //lanzado por el formulario cuando se seleccione 'Vender'.
+    sellForm.addEventListener("submit", function(e){
+
+        var productNameInput = document.getElementById("productName");
+
+        //Se realizan los controles necesarios, en este caso
+        //solamente se controla que se haya ingresado el nombre.
+        if (productNameInput.value == "")
+        {
+            productNameInput.classList.add('is-invalid');
+        }
+        else
+        {
+            //Aquí ingresa si pasó los controles, irá a enviar
+            //la solicitud para crear la publicación.
+            productNameInput.classList.remove('is-invalid');
+
+            getJSONData(URL_PUBLISH_PRODUCT).then(function(resultObj){
+                let msgToShowHTML = document.getElementById("resultSpan");
+                let msgToShow = "";
+    
+                //Si la publicación fue exitosa, devolverá mensaje de éxito,
+                //de lo contrario, devolverá mensaje de error.
+                if (resultObj.status = 'ok')
+                {
+                    msgToShow = SUCCESS_MSG;
+                    document.getElementById("alertResult").classList.add('alert-success');
+                }
+                else if (resultObj.status = 'error')
+                {
+                    msgToShow = ERROR_MSG;
+                    document.getElementById("alertResult").classList.add('alert-danger');
+                }
+    
+                msgToShowHTML.innerHTML = msgToShow;
+                document.getElementById("alertResult").classList.add("show");
+            });
+        }
+
+        if (e.preventDefault) e.preventDefault();
+            return false;
     });
 });
